ddc2973e5c5a7068315d39fcd0d17aff82bb7801
Gil Shelef <gilshe@il.ibm.com>
2017-06-02 18:23:02 +0300

    create notification when authorization email sent. Added non profit number in registration process.

--- app/src/main/java/com/gilshelef/feedme/nonprofit/activities/NonProfitRegistrationActivity.java
+++ app/src/main/java/com/gilshelef/feedme/nonprofit/activities/NonProfitRegistrationActivity.java
@@ -1,6 +1,7 @@
 package com.gilshelef.feedme.nonprofit.activities;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
@@ -8,7 +9,6 @@ import android.content.SharedPreferences;
 import android.content.res.Configuration;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -23,6 +23,7 @@ import com.gilshelef.feedme.nonprofit.data.OnBooleanResult;
 import com.gilshelef.feedme.nonprofit.data.OnResult;
 import com.gilshelef.feedme.util.Constants;
 import com.gilshelef.feedme.util.Logger;
+import com.gilshelef.feedme.util.Util;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -42,8 +43,10 @@ import java.util.Map;
 public class NonProfitRegistrationActivity extends AppCompatActivity implements View.OnClickListener, OnBooleanResult {
     private static final String TAG = NonProfitRegistrationActivity.class.getSimpleName();
     public static final int REGISTER_NON_PROFIT = 1;
-    private DatabaseReference mNonProfitRef;
+
+    private static DatabaseReference mNonProfitRef;
     private EditText mNonProfitName;
+    private EditText mNonProfitNumber;
     private EditText mNonProfitAddress;
     private EditText mContactFirstName;
     private EditText mContactLastName;
@@ -52,6 +55,8 @@ public class NonProfitRegistrationActivity extends AppCompatActivity implements 
     private LatLng mLatLng;
     private Logger mLogger;
     private String mNonProfitId;
+    private ProgressDialog mProgress;
+
 
 
     @Override
@@ -69,6 +74,7 @@ public class NonProfitRegistrationActivity extends AppCompatActivity implements 
         mContactLastName = (EditText) findViewById(R.id.non_profit_lname);
         mContactId = (EditText) findViewById(R.id.contact_id);
         mContactPhone = (EditText) findViewById(R.id.contact_phone);
+        mNonProfitNumber = (EditText) findViewById(R.id.non_profit_id);
         Button submitBtn = (Button) findViewById(R.id.submit_btn);
 
         // Click listeners
@@ -145,23 +151,17 @@ public class NonProfitRegistrationActivity extends AppCompatActivity implements 
     @Override
     public void onResult(boolean result) {
         if(result) {
-            notifyUser();
             writeNewNonProfit();
+            Util.notifyEmailSent(this, new DialogInterface.OnClickListener(){
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    if(mProgress != null && mProgress.isShowing())
+                        mProgress.dismiss();
+                    finish(RESULT_OK);
+                }
+            });
             mLogger.signUp(Logger.EVENT.NON_PROFIT, mNonProfitId);
         }
-    }
-
-    private void notifyUser() {
-        AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
-        alertDialog.setMessage(R.string.email_authorization_sent);
-        alertDialog.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener(){
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                finish(RESULT_OK);
-            }
-        });
-        alertDialog.setCancelable(false);
-        alertDialog.show();
     }
 
     private void writeNewNonProfit() {
@@ -190,10 +190,7 @@ public class NonProfitRegistrationActivity extends AppCompatActivity implements 
         editor.putBoolean(NonProfit.KEY_AUTHORIZED, false);
         editor.apply();
 
-
-
     }
-
 
     private class SendAuthorizationEmailTask extends AsyncTask<Void, Void, Void> {
         private String email;
@@ -203,15 +200,22 @@ public class NonProfitRegistrationActivity extends AppCompatActivity implements 
         private static final String E_NON_PROFIT_ID = "non_profit_id";
         private static final String E_NON_PROFIT = Constants.DB_NON_PROFIT;
         private static final String E_TO = "to";
+        private static final String E_NON_PROFIT_NUMBER = "non_profit_number";
 
-        SendAuthorizationEmailTask(Activity activity) {
+        SendAuthorizationEmailTask(OnBooleanResult callback) {
             this.email = Constants.DEFAULT_EMAIL;
-            this.callback = (OnBooleanResult) activity;
+            this.callback = callback;
+        }
+
+        @Override
+        protected void onPreExecute(){
+            mProgress = Util.buildProgressDialog(NonProfitRegistrationActivity.this);
+            mProgress.show();
         }
 
         @Override
         protected Void doInBackground(Void... params) {
-            final String nonProfitName = mNonProfitName.getText().toString().toLowerCase();
+            final String nonProfitNumber = mNonProfitNumber.getText().toString();
             final OnResult getEmailCallback = new OnResult() {
                 @Override
                 public void onResult() {
@@ -223,11 +227,11 @@ public class NonProfitRegistrationActivity extends AppCompatActivity implements 
             ValueEventListener getNonProfitEmail = new ValueEventListener() {
                 @Override
                 public void onDataChange(DataSnapshot dataSnapshot) {
-                    Log.d(TAG, "onDataChange");
+                    Log.d(TAG, "SendAuthorizationEmailTask: onDataChange");
                     for(DataSnapshot child: dataSnapshot.getChildren()){
-                        String name = child.child(E_CONTACT_NAME).getValue(String.class).toLowerCase();
-                        if(name.equals(nonProfitName)) {
-                            email = child.child("email").getValue(String.class);
+                        String id = child.getKey();
+                        if(id.equals(nonProfitNumber)) {
+                            email = child.getValue(String.class);
                             break;
                         }
                     }
@@ -246,6 +250,11 @@ public class NonProfitRegistrationActivity extends AppCompatActivity implements 
         }
 
         private void sendEmail() {
+
+            email = Constants.DEFAULT_EMAIL;
+            //TODO: remove line above
+
+
             //generate email information
             String contactName = mContactFirstName.getText().toString() + " " + mContactLastName.getText().toString();
             String contactId = mContactId.getText().toString();
@@ -256,6 +265,7 @@ public class NonProfitRegistrationActivity extends AppCompatActivity implements 
             updates.put(E_NON_PROFIT_ID, mNonProfitId);
             updates.put(E_TO, email);
             updates.put(E_NON_PROFIT, mNonProfitName.getText().toString());
+            updates.put(E_NON_PROFIT_NUMBER, mNonProfitNumber.getText().toString());
 
             //send email message
             DatabaseReference ref = FirebaseDatabase.getInstance().getReference().child(Constants.DB_EMAILS);
@@ -263,70 +273,4 @@ public class NonProfitRegistrationActivity extends AppCompatActivity implements 
             ref.child(key).updateChildren(updates);
         }
     }
-
-
-
-    //    private class SendAuthorizationEmail {
-//
-//        private final OnBooleanResult callback;
-//        private Context context;
-//        private String email;
-//
-//        public SendAuthorizationEmail(Activity activity) {
-//            this.context = activity;
-//            this.email = Constants.DEFAULT_EMAIL;
-//            this.callback = (OnBooleanResult) activity;
-//        }
-//
-//        public void execute() {
-//            final String nonProfitName = mNonProfitName.getText().toString().toLowerCase();
-//            final OnResult getEmailCallback = new OnResult() {
-//                @Override
-//                public void onResult() {
-//                    sendEmail();
-//                    callback.onResult(true);
-//                }
-//            };
-//
-//
-//            ValueEventListener getNonProfitEmail = new ValueEventListener() {
-//                @Override
-//                public void onDataChange(DataSnapshot dataSnapshot) {
-//                    Log.d(TAG, "onDataChange");
-//                    for(DataSnapshot child: dataSnapshot.getChildren()){
-//                        String name = child.child("name").getValue(String.class).toLowerCase();
-//                        if(name.equals(nonProfitName)) {
-//                            email = child.child("email").getValue(String.class);
-//                            break;
-//                        }
-//                    }
-//                    getEmailCallback.onResult();
-//                }
-//
-//                @Override
-//                public void onCancelled(DatabaseError databaseError) {
-//
-//                }
-//            };
-//
-//            DatabaseReference ref = FirebaseDatabase.getInstance().getReference().child(Constants.DB_NON_PROFIT_LISTED);
-//            ref.addListenerForSingleValueEvent(getNonProfitEmail);
-//        }
-//
-//        private void sendEmail() {
-//            DatabaseReference ref = FirebaseDatabase.getInstance().getReference().child("emails");
-//            String name = mContactFirstName.getText().toString() + " " + mContactLastName.getText().toString();
-//            String id = mContactId.getText().toString();
-//            mNonProfitId = mNonProfitRef.push().getKey();
-//
-//            Map<String, Object> updates = new HashMap<>();
-//            updates.put("name", name);
-//            updates.put("contact_id", id);
-//            updates.put("non_profit_id", mNonProfitId);
-//            updates.put("to", email);
-//            updates.put("non_profit", mNonProfitName.getText().toString());
-//            String key = ref.push().getKey();
-//            ref.child(key).updateChildren(updates);
-//        }
-//    }
 }
--- app/src/main/java/com/gilshelef/feedme/nonprofit/data/NonProfit.java
+++ app/src/main/java/com/gilshelef/feedme/nonprofit/data/NonProfit.java
@@ -1,11 +1,19 @@
 package com.gilshelef.feedme.nonprofit.data;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.location.Location;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
+import com.gilshelef.feedme.R;
 import com.gilshelef.feedme.launcher.RegistrationActivity;
+import com.gilshelef.feedme.nonprofit.activities.NonProfitMainActivity;
 import com.gilshelef.feedme.util.Constants;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.firebase.database.DataSnapshot;
@@ -91,6 +99,7 @@ public class NonProfit {
                 if(authorized) {
                     setAuthorized(context);
                     ref.removeEventListener(this);
+                    alertAuthorized(context);
                 }
             }
 
@@ -103,9 +112,31 @@ public class NonProfit {
         ref.addValueEventListener(listener);
     }
 
+    private void alertAuthorized(Context context) {
+        Intent intent = new Intent(context, NonProfitMainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0 /* Request code */, intent,
+                PendingIntent.FLAG_ONE_SHOT);
+
+        Uri defaultSoundUri= RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(context)
+                .setSmallIcon(R.mipmap.donate_icon)
+                .setContentTitle(context.getString(R.string.authrization_confimed))
+                .setContentText(context.getString(R.string.authrization_confimed_msg))
+                .setAutoCancel(true)
+                .setSound(defaultSoundUri)
+                .setContentIntent(pendingIntent);
+
+        NotificationManager notificationManager =
+                (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        notificationManager.notify(0 /* ID of notification */, notificationBuilder.build());
+
+    }
+
     private void setAuthorized(Context context) {
         Log.d(TAG, "non profit has been authorized!");
-        getEditor(context).putBoolean(KEY_AUTHORIZED, true);
+        getEditor(context).putBoolean(KEY_AUTHORIZED, true).apply();
         synchronized (this){
             this.authorized = true;
         }
